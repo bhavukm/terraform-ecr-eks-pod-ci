cbbc2e78277db97cc1b4d71119a60809b54ac7e5
Bhavuk Mudgal <53166534+bhavukm@users.noreply.github.com>
2025-12-16 13:14:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ data "aws_availability_zones" "available" {}
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 4.0"
+  version = "~> 5.0"
 
   name = "three-tier-vpc"
   cidr = "10.0.0.0/16"
